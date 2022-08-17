ac915c93047ee53e2fe0b78c852b1e7014d86db7
João Rocha <clariosmachine@hotmail.com>
2022-08-16 22:22:20 -0300

    add function checarIdade

--- js/script.js
+++ js/script.js
@@ -1,7 +1,35 @@
 var getTime = new Date()
-var anoCorrente = getTime.getFullYear()
-var anoNasc = document.querySelector('#formulario1')['anoNasc'].value
+const anoCorrente = getTime.getFullYear()
+var form = document.querySelector('#formulario1')
+var campoAnoNasc = document.querySelector('#formulario1')['anoNasc']
 /*de dentro do formulario podem ser pegos os campos através de uma busca de lista*/
-document.write(anoCorrente)
-var idade = (Number(anoCorrente) - Number(anoNasc))
-document.write(`<br>Idade = ${idade}`)
+
+var botaoChecar = document.querySelector('#botaoChecar')
+botaoChecar.addEventListener('click', checarIdade)
+
+function checarIdade() {
+    var anoNasc = campoAnoNasc.value
+    if (anoNasc == '') {  // verificando se o campo está em branco
+        window.alert('erro na idade');
+        return '';
+    }  // adicionar verificação de valores errados e alertas para o caso
+     
+    var genero = form.sexo.value
+    if (genero == '') {
+        window.alert('erro no genero');
+        return;  // o return sem nada faz a execução da função parar, como um break
+    }
+
+    var idade = (Number(anoCorrente) - Number(anoNasc))
+    divRes = document.createElement('div')
+    divRes.setAttribute('id', 'divRes')
+    divRes.innerHTML = `Sua idade é ${idade} e seu genero é ${genero}`
+    form.appendChild(divRes)
+    botaoChecar.setAttribute('disabled', '')  // desabilita o botão
+}
+
+/*
+pode ser adicionado um botão de resete que limparia os valores
+de genero e ano de nascimento, além de habilitar o botão de checar
+e sumir
+*/
