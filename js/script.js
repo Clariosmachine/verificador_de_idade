577d29edc2b002f48bf6111b85c1ff358fbf6293
João Rocha <clariosmachine@hotmail.com>
2022-08-19 00:30:52 -0300

    adição das imagens

--- js/script.js
+++ js/script.js
@@ -10,21 +10,54 @@ botaoChecar.addEventListener('click', checarIdade)
 function checarIdade() {
     var anoNasc = campoAnoNasc.value
     if (anoNasc == '') {  // verificando se o campo está em branco
-        window.alert('erro na idade');
+        window.alert('Idade digitada inválida!');
         return '';
     }  // adicionar verificação de valores errados e alertas para o caso
      
     var genero = form.sexo.value
     if (genero == '') {
-        window.alert('erro no genero');
+        window.alert('Selecione um dos gêneros!');
         return;  // o return sem nada faz a execução da função parar, como um break
     }
 
     var idade = (Number(anoCorrente) - Number(anoNasc))
+    
     divRes = document.createElement('div')
     divRes.setAttribute('id', 'divRes')
-    divRes.innerHTML = `Sua idade é ${idade} e seu genero é ${genero}`
-    form.appendChild(divRes)
+    
+    if (genero == 'feminino') {
+        if (idade >= 0 && idade <= 17) {
+            divRes.innerHTML = `<p>Pessoa identificada como uma menina com ${idade} anos de idade.</p>
+            <img src="/imgs/menina.png" alt="Imagem de Menina">`
+        }
+        else if (idade >= 18 && idade <= 59) {
+            divRes.innerHTML = `<p>Pessoa identificada como uma mulher com ${idade} anos de idade.</p>
+            <img src="/imgs/adulta.png" alt="Imagem de Mulher">`
+        }
+        else {
+            divRes.innerHTML = `<p>Pessoa identificada como uma idosa com ${idade} anos de idade.</p>
+            <img src="/imgs/idosa.png" alt="Imagem de Idosa">`
+        }
+    }
+    
+    else {
+        if (idade >= 0 && idade <= 17) {
+            divRes.innerHTML = `<p>Pessoa identificada como um menino com ${idade} anos de idade.</p>
+            <img src="/imgs/menino.png" alt="Imagem de Menino">`
+        }
+        else if (idade >= 18 && idade <= 59) {
+            divRes.innerHTML = `<p>Pessoa identificada como um homem com ${idade} anos de idade.</p>
+            <img src="/imgs/adulto.png" alt="Imagem de Homem">`
+        }
+        else {
+            divRes.innerHTML = `<p>Pessoa identificada como um idoso com ${idade} anos de idade.</p>
+            <img src="/imgs/idoso.png" alt="Imagem de Idoso">`
+        }
+    }
+
+    /*divRes.innerHTML = `<p>Sua idade é ${idade} e seu genero é ${genero}</p>`*/
+    document.getElementsByTagName('body')[0].appendChild(divRes)
+    
     botaoChecar.setAttribute('disabled', '')  // desabilita o botão
 }
 
